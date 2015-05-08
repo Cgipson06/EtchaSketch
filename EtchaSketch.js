9c3b8ff5d974acf0b28819b86182a65a66b99642
Cody Gipson <CGipson06@yahoo.com>
2015-05-07 21:51:31 -0500

    Final Commit

--- EtchaSketch.js
+++ EtchaSketch.js
@@ -1,13 +1,12 @@
-//Set up the initial grid of div 
+
 numOfSquares = 16;
 count = 0;  
-containerDimensions= 400;
 
 $(document).ready( function () {
 
-	$('body').append('<button type="button">Reset the sceen</button>');
+	$('body').append('<button type="button" id="button">Reset the sceen</button>');
 	$('body').append('<div id="container"></div>');
-	
+
 
 	function display() {
 		while (count<numOfSquares) {
@@ -21,34 +20,27 @@ $(document).ready( function () {
 		};
 	};
 	display();
-//Reset the screen after clicking the reset button	
-	
 	$(document).on('click',':button', function screenReset() {
-		numOfSquares= prompt("Please input the size of the new screen.","16");
+		numOfSquares= prompt("Please input the size of the new screen. 1 - 128","16");
+		while (numOfSquares<1 || numOfSquares >128) {
+			numOfSquares= prompt("Please input the size of the new screen.1 - 128","16");
+		}
 		$('#container').empty();
 		$(".screen").css('background-color','#454343');
 		dimension = 500/numOfSquares;
-		console.log(dimension);
 		count=0;
 		display();
 		$(".screen").css('height',dimension);		
 		$(".screen").css('width',dimension);
 		shader();
 	});
-	
-//Basic shading of the pixel we hover over
 	function shader() {
 		$('.screen').hover(function () {
 			$(this).css('background-color', "#000");
-		//this should be changed to opacity and gradually lightened to show the black background more and more.
 		});
 	};
 	shader();
-	
-	
-
-//Append the footer 
-	$('body').append('<footer><p><small>Made by Cody Gipson</small></p></footer>');
+	$('body').append('<footer><p><small>CGipson 2015</small></p></footer>');
 
 });
 	
